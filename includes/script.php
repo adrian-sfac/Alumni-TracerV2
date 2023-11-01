dde905f9878cae931b5348a4a29fbbe5675b3769
Drain Fox <adrian-sfac>
2023-11-01 18:27:38 +0800

    online forum working, still needs automatic user detection

--- includes/script.php
+++ includes/script.php
@@ -3,8 +3,7 @@
 <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js
-"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
 <script src="../../assets/js/core/popper.min.js"></script>
 <script src="../../assets/js/core/bootstrap.min.js"></script>
 <script src="../../assets/js/plugins/perfect-scrollbar.min.js"></script>
--- pages/forum/forum-form.php
+++ pages/forum/forum-form.php
@@ -5,19 +5,28 @@
 <title>Discussion Forum</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 	<script src="main.js"></script>
 </head>
 
-<!-- Modal -->
-<div id="ReplyModal" class="modal fade" role="dialog">
+<?php
+include '../../includes/session.php';
+// End Session
+include '../../includes/head.php';
+?>
+
+<body class="g-sidenav-show  bg-gray-200">
+  
+<?php include "../../includes/sidebar.php"?>
+
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+<div id="ReplyModal" class="modal" role="dialog">
   <div class="modal-dialog">
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <button type="button" class="close" style="border: 1px solid black; height: 35px; border-radius: 10px; padding: 5px;" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Reply Question</h4>
       </div>
       <div class="modal-body">
@@ -25,54 +34,56 @@
             <input type="hidden" id="commentid" name="Rcommentid">
         	<div class="form-group">
         	  <label for="usr">Write your name:</label>
-        	  <input type="text" class="form-control" name="Rname" required>
+        	  <input type="text" class="form-control" style="border: 1px solid black; border-radius: 10px; padding: 10px; margin-bottom: 10px;" name="Rname" required>
         	</div>
             <div class="form-group">
               <label for="comment">Write your reply:</label>
-              <textarea class="form-control" rows="5" name="Rmsg" required></textarea>
+              <textarea class="form-control" style="border: 1px solid black; border-radius: 10px; padding: 10px; margin-bottom: 25px;" rows="5" name="Rmsg" required></textarea>
             </div>
-        	 <input type="button" id="btnreply" name="btnreply" class="btn btn-primary" value="Reply">
+        	 <input type="button" id="btnreply" name="btnreply" class="btn btn-dark" value="Reply">
       </form>
       </div>
     </div>
 
   </div>
 </div>
+</div>
 
-<div class="container">
-
-<div class="panel panel-default" style="margin-top:50px">
-  <div class="panel-body">
-    <h3>Discussion Forum</h3>
-    <hr>
-    <form name="frm" method="post">
-        <input type="hidden" id="commentid" name="Pcommentid" value="0">
-	<div class="form-group">
-	  <label for="usr">Write your name:</label>
-	  <input type="text" class="form-control" name="name" required>
-	</div>
-    <div class="form-group">
-      <label for="comment">Write your question:</label>
-      <textarea class="form-control" rows="5" name="msg" required></textarea>
+<div class="container mt-5">
+    <div class="card">
+        <div class="card-header bg-dark text-white">
+            <h3 class="mb-0 text-white">Discussion Forum</h3>
+        </div>
+        <div class="card-body">
+            <form name="frm" method="post">
+                <input type="hidden" id="commentid" name="Pcommentid" value="0">
+                <div class="form-group mb-2 text-black">
+                    <label for="usr">Your Name:</label>
+                    <input type="text" class="form-control" style="border: 1px solid black; border-radius: 10px; padding: 10px;" name="name" required>
+                </div>
+                <div class="form-group mb-3 text-black">
+                    <label for="comment">Your Question:</label>
+                    <textarea class="form-control" style="border: 1px solid black; border-radius: 10px; padding: 10px;" rows="5" name="msg" required></textarea>
+                </div>
+                <button type="button" id="butsave" name="save" class="btn btn-dark">Send</button>
+            </form>
+        </div>
     </div>
-	 <input type="button" id="butsave" name="save" class="btn btn-primary" value="Send">
-  </form>
-  </div>
-</div>
-  
 
-<div class="panel panel-default">
-  <div class="panel-body">
-    <h4>Recent Questions</h4>           
-	<table class="table" id="MyTable" style="background-color: #edfafa; border:0px;border-radius:10px">
-	  <tbody id="record">
-		
-	  </tbody>
-	</table>
-  </div>
+    <div class="card mt-3">
+        <div class="card-body">
+            <h4>Recent Questions</h4>           
+            <table class="table" id="MyTable" style="background-color: #fff; border:0px;border-radius:10px">
+                <tbody id="record">
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
-</div>
-
+<?php include "../../includes/footer.php"?>
 </body>
 </html>
+<!--   Core JS Files   -->
+<?php include "../../includes/script.php"?>
