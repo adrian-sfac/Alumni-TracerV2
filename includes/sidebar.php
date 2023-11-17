33fcf9aafc68fe80fb648ea5c7de1b8840f7aa10
Drain Fox <adrian-sfac>
2023-11-18 05:14:40 +0800

    sidebar admin added add news, i forgot.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -488,6 +488,14 @@
                   <span class="nav-link-text ms-1  ps-1">Add Job Form</span>
                 </a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link text-white " href="../news/news-form.php">
+                  <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                  <i class="bi bi-newspaper"></i>
+                  </div>
+                  <span class="nav-link-text ms-2 ps-1">Add News</span>
+                </a>
+              </li>
               <li class="nav-item ">
                 <a class="nav-link text-white " href="../donation/donation-form.php">
                   <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
