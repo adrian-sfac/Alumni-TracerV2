5547aa5b855404d4adf1a0b4d2e3fd6e890a97ed
Seuzen Hoffer <adrian-sfac>
2024-01-05 03:06:41 +0800

    added person to contact on donation details

--- pages/donation/donation-form.php
+++ pages/donation/donation-form.php
@@ -51,33 +51,59 @@ include '../../includes/head.php';
 </div> -->
 
 <div class="container-fluid py-2">
-        <div class="row mt-1 justify-content-center">
-            <div class="col-md-6">
-                <div class="card px-3 pb-3">
-                    <div class="col-md-12">
-                            <h3 class="text-center mt-3 mb-2">Institution</h3>
-                            <p style="text-align: justify; margin: 10px 30px; color: black; font-size: 20px; line-height: 1.6;">
-                            Eastwest Bank Deposit/Mobile Transfer <br>
-                            Bank Account: Eastwest Bank Deposit <br>
-                            Account Name: Saint Francis of Assisi College of Cavite <br>
-                            Account Number: 2000-0271-4719 <br>
-                            Branch: Molino Boulevard <br> <br>
-                            
-                            Gcash Online Fund Transfer <br>
-                            Bank: EastWest Bank <br>
-                            Account Name: SFAC Bacoor Campus <br>
-                            Account Number: 2000-0271-4719
-                            </p>
+    <div class="row mt-1 justify-content-center">
+        <div class="col-md-6 mb-3">
+            <div class="card px-3 pb-3">
+                <div class="col-md-12">
+                    <h3 class="text-center mt-3 mb-2">Institution</h3>
+                    <p style="text-align: justify; margin: 10px 30px; color: black; font-size: 20px; line-height: 1.6;">
+                        Eastwest Bank Deposit/Mobile Transfer <br>
+                        Bank Account: Eastwest Bank Deposit <br>
+                        Account Name: Saint Francis of Assisi College of Cavite <br>
+                        Account Number: 2000-0271-4719 <br>
+                        Branch: Molino Boulevard <br> <br>
+                        
+                        Gcash Online Fund Transfer <br>
+                        Bank: EastWest Bank <br>
+                        Account Name: SFAC Bacoor Campus <br>
+                        Account Number: 2000-0271-4719
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card px-3 pb-3">
+                <h3 class="text-center mt-3 mb-2">Sir Ralph Arjay Dela Cruz</h3>
+                <div class="row align-items-center">
+                    <div class="col-md-6 align-self-center text-center">
+                        <img src="sir-ralph.jpg" style="height: 330px; padding: 9px; border-radius: 20px;" alt="default.jpg" class="img img-fluid">
+                    </div>
+                    <div class="col-md-6">
+                        <p style="text-align: justify; text-indent: 7%; margin-top: 6%; margin-left: 6%; margin-right: 6%; color: black; font-size: 20px;">
+                            In case of other donation concerns, you can contact this person using these provided links. <br><br>
+                        </p>
+                        <div class="d-flex justify-content-center flex-wrap">
+                            <a href="https://www.facebook.com/arjay.cruz.961" target="_blank" class="btn btn-dark m-2">
+                                <i class="bi bi-facebook" style="font-size: 24px;"></i>
+                            </a>
+                            <a href="mailto:info@stfrancis.edu.ph" target="_blank" class="btn btn-dark m-2">
+                                <i class="bi bi-envelope-at-fill" style="font-size: 24px;"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div>  
+</div>
+
+
+
 
     <div class="container-fluid py-2">
         <div class="row mt-1 justify-content-center">
-            <div class="col-md-6">
+            <div class="col-md-8">
                 <div class="card px-3 pb-3">
                     <div class="col-md-12">
                             <h3 class="text-center mt-3 mb-2">Thank You!</h3>
